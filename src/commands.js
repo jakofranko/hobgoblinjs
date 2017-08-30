cb2cab4ec972b3e300a72e0fedac09babe2a25b1
Jake Franklin <jacob.d.franklin@gmail.com>
2017-08-30 12:08:48 -0700

    Continued work on the new input handling system

--- src/commands.js
+++ src/commands.js
@@ -6,14 +6,46 @@
 // and return nothing. With a little bit of support for binding functions to commands, this may be all we
 // need. However, another way this can be implemented is to have a command RETURN a function, so that commands
 // can be manipulated independently. I think I will go with this approach, since it can allow an entity to
-// undo/redo actions, as well as allow us to decouple the entity and the command, allowing the player to 
+// undo/redo actions, as well as allow us to decouple the entity and the command, allowing the player to
 // control monsters and vice-versa.
+//
+// There are two other variations of this pattern that are relevant to our purposes. The first is to have
+// the returned function take an actor (entity) as a parameter. This let's us perform the command on any
+// actor we desire at a later time. The other variation binds the entity to the returned function. The
+// advantage of this set up is that it would allow undo/redo behavior with less overhead. For now, I
+// think that the first variety will be fine for our needs, since keeping track of a stream of commands
+// that can be undone/redone isn't a big feature for most roguelikes, and could be implemented with little
+// difficulty for one that wanted to support this functionality.
 //
 // This architecture will involve two parts: 1) a 'handler' (for now, player input) that will take an input,
 // and return a command to be executed later, and 2) a list of commands.
 
 Game.Commands = {};
 
-Game.Commands.moveCommand = function(entity, x, y, z) {
+Game.Commands.moveCommand = function(diffX, diffY, diffZ) {
+    return function(entity) {
+        entity.tryMove(entity.getX() - diffX, entity.getY() - diffY, entity.getZ() - diffZ);
+    };
+};
 
+Game.Commands.showScreenCommand = function(screen) {
+    return function() {
+        throw new Error('Must be defined');
+    };
+};
+
+Game.Commands.showItemScreenCommand = function(screen) {
+    return function() {
+        throw new Error('Must be defined');
+    };
+};
+
+Game.Commands.showTargettingScreenCommand = function(screen) {
+    return function() {
+        throw new Error('Must be defined');
+    };
+};
+
+Game.Commands.nullCommand = function() {
+    return function(){};
 };
--- src/input.js
+++ src/input.js
@@ -1,115 +1,40 @@
 Game.Input = {};
 
-Game.Input.handleInput = function(inputType, inputData) {
-    var keyCode = inputData.keyCode;
-    switch(keycode) {
-        case ROT.VK_LEFT:
-            break;
-        case ROT.VK_RIGHT:
-            break;
-        case ROT.VK_UP:
-            break;
-        case ROT.VK_DOWN:
-            break;
-        case ROT.VK_I:
-            break;
-        case ROT.VK_D:
-            break;
-        case ROT.VK_E:
-            break;
-        case ROT.VK_W:
-            break;
-        case ROT.VK_X:
-            break;
-        case ROT.VK_T:
-            break;
-        case ROT.VK_COMMA:
-            break;
-        case ROT.VK_SPACE:
-            break;
+Game.Input.controlMaps = {};
+
+// Defining controlMaps like this should, theoretically, allow key bindings for each screen
+// to be re-configured on the fly; simply say:
+//
+// `Game.Input.controlMaps[screen][inputType][input] = someFunc.bind(this, params);`
+//
+// and ker-blamo.
+// These controlMaps could/should be defined from each screen? Maybe not
+Game.Input.controlMaps.playScreen = {
+    keydown: {
+        'ArrowLeft':    Game.Commands.moveCommand.bind(this, -1, 0, 0),
+        'ArrowRight':   Game.Commands.moveCommand.bind(this, 1, 0, 0),
+        'ArrowUp':      Game.Commands.moveCommand.bind(this, 0, -1, 0),
+        'ArrowDown':    Game.Commands.moveCommand.bind(this, 0, 1, 0),
+        '>':            Game.Commands.moveCommand.bind(this, 0, 0, -1),
+        '<':            Game.Commands.moveCommand.bind(this, 0, 0, 1),
+        'Space':        Game.Commands.showItemScreenCommand.bind(this, Game.Screen.actionScreen),
+        'i':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.inventoryScreen),
+        'd':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.dropScreen),
+        'e':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.eatScreen),
+        'w':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.wieldScreen),
+        'W':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.wearScreen),
+        'x':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.examineScreen),
+        't':            Game.Commands.showItemScreenCommand.bind(this, Game.Screen.throwScreen),
+        ',':            Game.Commands.nullCommand.bind(this), // Should potentially show a get screen
+        ';':            Game.Commands.showTargettingScreenCommand.bind(this, Game.Screen.lookScreen),
+        '?':            Game.Commands.showScreenCommand.bind(this, Game.Screen.helpScreen)
     }
-    if (inputType === 'keydown') {
-        if (inputData.keyCode === ROT.VK_LEFT) {
-            this.move(-1, 0, 0);
-        } else if (inputData.keyCode === ROT.VK_RIGHT) {
-            this.move(1, 0, 0);
-        } else if (inputData.keyCode === ROT.VK_UP) {
-            this.move(0, -1, 0);
-        } else if (inputData.keyCode === ROT.VK_DOWN) {
-            this.move(0, 1, 0);
-        } else if (inputData.keyCode === ROT.VK_I) {
-            // Show the inventory screen
-            this.showItemsSubScreen(Game.Screen.inventoryScreen, this._player.getItems(), 'You are not carrying anything.');
-            return;
-        } else if (inputData.keyCode === ROT.VK_D) {
-            // Show the drop screen
-            this.showItemsSubScreen(Game.Screen.dropScreen, this._player.getItems(), 'You have nothing to drop.');
-            return;
-        } else if (inputData.keyCode === ROT.VK_E) {
-            // Show the drop screen
-            this.showItemsSubScreen(Game.Screen.eatScreen, this._player.getItems(), 'You have nothing to eat.');
-            return;
-        } else if (inputData.keyCode === ROT.VK_W) {
-            if (inputData.shiftKey) {
-                // Show the wear screen
-                this.showItemsSubScreen(Game.Screen.wearScreen, this._player.getItems(), 'You have nothing to wear.');
-            } else {
-                // Show the wield screen
-                this.showItemsSubScreen(Game.Screen.wieldScreen, this._player.getItems(), 'You have nothing to wield.');
-            }
-        } else if (inputData.keyCode === ROT.VK_X) {
-            // Show the drop screen
-            this.showItemsSubScreen(Game.Screen.examineScreen, this._player.getItems(),
-               'You have nothing to examine.');
-            return;
-        } else if(inputData.keyCode === ROT.VK_T) {
-            this.showItemsSubScreen(Game.Screen.throwScreen, this._player.getItems(), 'You have nothing to throw.');
-            return;
-        } else if (inputData.keyCode === ROT.VK_COMMA) {
-            var items = this._player.getMap().getItemsAt(this._player.getX(), this._player.getY(), this._player.getZ());
-            // If there is only one item, directly pick it up
-            if (items && items.length === 1) {
-                var item = items[0];
-                if (this._player.pickupItems([0])) {
-                    Game.sendMessage(this._player, "You pick up %s.", [item.describeA()]);
-                } else {
-                    Game.sendMessage(this._player, "Your inventory is full! Nothing was picked up.");
-                }
-            } else {
-                this.showItemsSubScreen(Game.Screen.pickupScreen, items, 'There is nothing here to pick up.');
-            }
-        } else if(inputData.keyCode === ROT.VK_SPACE) {
-            Game.Screen.actionMenu.setup(this._player);
-            this.setSubScreen(Game.Screen.actionMenu);
-        } else {
-            // Not a valid key
-            return;
-        }
-        // Unlock the engine
-        this._player.getMap().getEngine().unlock();
-    } else if (inputType === 'keypress') {
-        var keyChar = String.fromCharCode(inputData.charCode);
-        if(keyChar === '>') {
-            this.move(0, 0, 1);
-        } else if(keyChar === '<') {
-            this.move(0,0, -1);
-        } else if (keyChar === ';') {
-            // Setup the look screen.
-            var offsets = this.getScreenOffsets();
-            Game.Screen.lookScreen.setup(this._player,
-                this._player.getX(), this._player.getY(),
-                offsets.x, offsets.y);
-            this.setSubScreen(Game.Screen.lookScreen);
-            return;
-        } else if (keyChar === '?') {
-            // Setup the look screen.
-            this.setSubScreen(Game.Screen.helpScreen);
-            return;
-        } else {
-            // Not a valid key
-            return;
-        }
-        // Unlock the engine
-        this._player.getMap().getEngine().unlock();
-    }
-}
+};
+
+// This function is meant to handle input data of all types
+Game.Input.handleInput = function(screen, inputType, inputData) {
+    // Each keyMap object should contain a list of references to Commands with specific parameters
+    // bound to them. These command functions will return a function that can be executed later,
+    // by passing in a specific entity to the function returned from `handleInput`
+    return Game.Input.controlMaps[screen][inputType][inputData.key]();
+};
