c87de85be6e9c18fecbe64c3daef50a801e1f21c
Jake Franklin <jacob.d.franklin@gmail.com>
2017-12-05 22:16:43 -0700

    Test script for automating changes

---
+++ windows_test.bat
@@ -0,0 +1,15 @@
+ECHO ON
+TITLE Automated Test Build for hobgoblin
+IF NOT EXIST test (mkdir test)
+SET /P _deltest= Clear contents of test dir? (y/n)
+IF "%_deltest%"=="y" GOTO :deltest
+IF "%_deltest%"=="n" GOTO :cdtest
+:deltest
+del test\js\*.js
+rmdir test\js
+:cdtest
+ECHO Entering test dir...
+cd test
+node ..\hobgoblin.js init -e
+index.html
+cd ..
